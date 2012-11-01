417d7adc4e827e9847f9621f8169ee760ee150ef
Evelyn Huynh <ehuynh@agamatrix.com>
2012-11-01 09:30:40 -0400

    IJB-1: First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IjbVietnam::Application.config.secret_token = '921ad62e0a701168095a3ecdf655949018a5f7cfff9cd2036e392a54b87dd0545db491520b1f875783872fb90f93a415d514c0e5d99f9e922e83b42538022308'
